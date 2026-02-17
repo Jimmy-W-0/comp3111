142ebd332908dace0488f9a2de27292826889534
Jimmy <zwucu@connect.ust.hk>
2026-02-17 23:42:40 +0800

    change the myAPP file

--- src/main/java/lab1/myAPP.java
+++ src/main/java/lab1/myAPP.java
@@ -6,7 +6,7 @@ public class myAPP {
     int num1=2;
     int exp = 4;
 
-
+        System.out.println("hi");
     System.out.println("Factorial of "+num1+":"+ MyLibrary.factorial(num1) );
     System.out.println(" "+num1+" power of "+ exp +" will be "+ MyLibrary.Power(num1,exp) );
         System.out.println("Done !!!!");
