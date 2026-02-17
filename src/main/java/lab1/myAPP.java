ae11687cf4761984fa747ae6fc660b6e031f8a04
Jimmy <zwucu@connect.ust.hk>
2026-02-17 23:40:40 +0800

    CRREATE lab 1 project

---
+++ src/main/java/lab1/myAPP.java
@@ -0,0 +1,14 @@
+package lab1;
+import lab1.MyLibrary;
+public class myAPP {
+    public static void main(String[] args) {
+
+    int num1=2;
+    int exp = 4;
+
+
+    System.out.println("Factorial of "+num1+":"+ MyLibrary.factorial(num1) );
+    System.out.println(" "+num1+" power of "+ exp +" will be "+ MyLibrary.Power(num1,exp) );
+        System.out.println("Done !!!!");
+    }
+}
